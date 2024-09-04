c7091782549d028b80904c148aeeebe9a6e4c0a7
r1-202 <khmkhm626@gmail.com>
2024-09-04 13:17:28 +0300

    added glad and glm libraries

--- BERender/include/Camera.h
+++ BERender/include/Camera.h
@@ -1,6 +1,16 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+#include <iostream>
 
+#include <vector>
+
+namespace BERender
+{
+  using namespace glm;
+}
 
 #endif //CAMERA_H
