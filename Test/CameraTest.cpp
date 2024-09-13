4f3e703990ba5134bf7c1f3dfdb1b0565be8db80
unknown <khmkhm626@gmail.com>
2024-09-13 13:04:59 +0300

    added glfw

--- Test/CameraTest.cpp
+++ Test/CameraTest.cpp
@@ -3,5 +3,12 @@
 int main()
 {
   BERender::Camera camera = BERender::Camera();
-  std::cout<<"Hi";
+  float prev = time(0);
+  while(true)
+  {
+    camera.processKeyboard(BERender::Direction::FORWARD,time(0)-prev);
+    _sleep(100);
+    prev = time(0);
+    std::cout<<camera.position.x<<' '<<camera.position.y<<' '<<camera.position.z<<'\n';
+  }
 }
