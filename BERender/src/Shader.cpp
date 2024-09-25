7f188a52a77af55749df6ac5d85994cbe0670f9f
r1-202 <khmkhm626@gmail.com>
2024-09-25 11:25:18 +0300

    implemented shader uniform setting

--- BERender/src/Shader.cpp
+++ BERender/src/Shader.cpp
@@ -67,3 +67,48 @@ Shader::Shader(const char *vertex_path, const char *fragment_path)
   glDeleteShader(vertex);
   glDeleteShader(fragment);
 }
+
+void Shader::use()
+{
+  glUseProgram(ID);
+}
+
+void Shader::setBool(const std::string &name, bool value) const
+{
+  glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+}
+
+void Shader::setInt(const std::string &name, int value) const
+{
+  glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+void Shader::setFloat(const std::string &name, float value) const
+{
+  glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+void Shader::setMat4(const std::string &name, glm::mat4 value) const
+{
+  glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, false, glm::value_ptr(value));
+}
+
+void Shader::setVec3(const std::string &name, glm::vec3 value) const
+{
+  glUniform3f(glGetUniformLocation(ID, name.c_str()), value.x, value.y, value.z);
+}
+
+void Shader::setVec3(const std::string &name, float x, float y, float z) const
+{
+  glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
+}
+
+void Shader::setVec2(const std::string &name, glm::vec2 value) const
+{
+  glUniform2f(glGetUniformLocation(ID, name.c_str()), value.x, value.y);
+}
+
+void Shader::setVec2(const std::string &name, float x, float y) const
+{
+  glUniform2f(glGetUniformLocation(ID, name.c_str()), x, y);
+}
