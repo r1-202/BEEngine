09a8f3bcd02c4e4d2944cb2457bf88ab0215d3e3
r1-202 <khmkhm626@gmail.com>
2024-09-27 05:45:47 +0300

    created vertex class which holds interleaved values for positions, normals, and texture coordinates

---
+++ BERender/include/Vertex.h
@@ -0,0 +1,27 @@
+#ifndef VERTEX_H
+#define VERTEX_H
+
+#include <glm/glm.hpp>
+
+namespace BERender
+{
+  class Vertex
+  {
+  public:
+    glm::vec3 position;
+    glm::vec3 normal;
+    glm::vec2 texture_coordinates;
+  };
+
+  class Vertextnbtn
+  {
+  public:
+    glm::vec3 position;
+    glm::vec3 normal;
+    glm::vec2 texture_coordinates;
+    glm::vec3 tangent;
+    glm::vec3 bitangent;
+  };
+}
+
+#endif // VERTEX_H
