0f4fc6ff97f11ae7a2b5440a1c3c4df5fd6655e6
r1-202 <khmkhm626@gmail.com>
2024-09-29 18:42:46 +0300

    Created VKBERenderer which will include header and source files for a Vulkan based renderer which will be build using glfw.

---
+++ VKBERender/include/Camera.h
@@ -0,0 +1,39 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+#include <vector>
+#include <Directions.h>
+
+namespace BERender
+{
+  class Camera
+  {
+  public:
+    glm::vec3 position;
+    glm::vec3 front;
+    glm::vec3 up;
+    glm::vec3 right;
+    glm::vec3 world_up;
+
+    float yaw;
+    float pitch;
+
+    float movement_speed;
+    float mouse_sensitivity;
+    float zoom;
+
+    Camera(glm::vec3 position = glm::vec3(0, 0, 0), glm::vec3 up = glm::vec3(0, 1, 0), float yaw = -90.0f, float pitch = 0.0f);
+    glm::mat4 getViewMatrix();
+    void processKeyboard(Direction direction, float dt);
+    void processMouseMovement(float xoffset, float yoffset, bool constraint_pitch = true);
+    void processMouseScroll(float yoffset);
+
+  private:
+    void updateCameraVectors();
+  };
+}
+
+#endif // CAMERA_H
---
+++ VKBERender/include/Directions.h
@@ -0,0 +1,17 @@
+#ifndef DIRECTIONS_H
+#define DIRECTIONS_H
+
+namespace BERender
+{
+  enum Direction
+  {
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN
+  };
+}
+
+#endif // DIRECTIONS_H
