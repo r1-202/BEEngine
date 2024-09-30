ecb5f3c542b7aa2a7bdf36bbd2f2ae70a4763719
r1-202 <khmkhm626@gmail.com>
2024-09-30 15:18:46 +0300

    Instance

--- Test/VKDrawTriangle.cpp
+++ Test/VKDrawTriangle.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
 
 const unsigned int WIDTH = 800;
 const unsigned int HEIGHT = 600;
@@ -13,6 +14,7 @@ class HelloTriangleApplication
 {
 private:
   GLFWwindow *window;
+  VkInstance instance;
 
 public:
   void run()
@@ -31,20 +33,64 @@ private:
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
     window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
   }
+
   void initVulkan()
   {
+    createInstance();
   }
+
   void mainLoop()
   {
-    while(!glfwWindowShouldClose(window))
+    while (!glfwWindowShouldClose(window))
     {
       glfwPollEvents();
     }
   }
+
   void cleanup()
   {
+    vkDestroyInstance(instance, nullptr);
     glfwDestroyWindow(window);
     glfwTerminate();
+  }
+
+  void createInstance()
+  {
+    VkApplicationInfo app_info{};
+    app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    app_info.pApplicationName = "Hello Triangle";
+    app_info.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    app_info.pEngineName = "No Engine";
+    app_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    app_info.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo create_info{};
+    create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    create_info.pApplicationInfo = &app_info;
+    unsigned int glfw_extension_count = 0;
+    const char **glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extension_count);
+
+    std::vector<const char *> required_extensions;
+
+    for (unsigned int i = 0; i < glfw_extension_count; ++i)
+    {
+      required_extensions.emplace_back(glfw_extensions[i]);
+    }
+
+#ifdef __APPLE__
+    required_extensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+
+    create_info.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+
+#endif
+
+    create_info.enabledExtensionCount = (unsigned int)required_extensions.size();
+    create_info.ppEnabledExtensionNames = required_extensions.data();
+
+    if (vkCreateInstance(&create_info, nullptr, &instance) != VK_SUCCESS)
+    {
+      throw std::runtime_error("failed to create instance!");
+    }
   }
 };
 
