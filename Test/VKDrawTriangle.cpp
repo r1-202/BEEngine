01fc62699613c573e045942712576e7a101a0811
r1-202 <khmkhm626@gmail.com>
2024-09-30 13:06:54 +0300

    base code

--- Test/VKDrawTriangle.cpp
+++ Test/VKDrawTriangle.cpp
@@ -1,28 +1,50 @@
 #include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <glfw3.h>
 
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
 
+const unsigned int WIDTH = 800;
+const unsigned int HEIGHT = 600;
+
 class HelloTriangleApplication
 {
+private:
+  GLFWwindow *window;
+
 public:
   void run()
   {
+    initWindow();
     initVulkan();
     mainLoop();
     cleanup();
   }
 
 private:
+  void initWindow()
+  {
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+  }
   void initVulkan()
   {
   }
   void mainLoop()
   {
+    while(!glfwWindowShouldClose(window))
+    {
+      glfwPollEvents();
+    }
   }
   void cleanup()
   {
+    glfwDestroyWindow(window);
+    glfwTerminate();
   }
 };
 
