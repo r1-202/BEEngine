3a4f9599f741b249c095e60f93a15ed4f9449c0c
r1-202 <khmkhm626@gmail.com>
2024-09-30 15:38:42 +0300

    implemented methods to parse tokens, floats, and ints

--- Parser/include/Parser.h
+++ Parser/include/Parser.h
@@ -5,4 +5,8 @@
 namespace Parser
 {
   void splitTail(std::string &full, std::string &tail, std::string &rest);
+  void clearSpace(std::string &buffer, int &i);
+  std::string getNextToken(std::string &buffer, int &i);
+  float getNextFloat(std::string &buffer, int &i);
+  int getNextInt(std::string &buffer, int &i);
 }
--- Parser/src/Parser.cpp
+++ Parser/src/Parser.cpp
@@ -5,7 +5,7 @@ void Parser::splitTail(std::string &full, std::string &tail, std::string &rest)
   tail = "";
   rest = "";
   int i;
-  for (int i = full.size() - 1; i > -0; --i)
+  for (int i = full.size() - 1; i >= 0; --i)
   {
     if (full[i] == '/')
       break;
@@ -18,4 +18,36 @@ void Parser::splitTail(std::string &full, std::string &tail, std::string &rest)
   {
     rest += full[i];
   }
+}
+
+void Parser::clearSpace(std::string &buffer, int &i)
+{
+  while (buffer[i] == ' ' && i < buffer.size())
+    ++i;
+}
+
+std::string Parser::getNextToken(std::string &buffer, int &i)
+{
+  clearSpace(buffer, i);
+  std::string token = "";
+  while (buffer[i] != ' ' && i < buffer.size())
+  {
+    token += buffer[i];
+    ++i;
+  }
+  return token;
+}
+
+float Parser::getNextFloat(std::string &buffer, int &i)
+{
+  clearSpace(buffer, i);
+  std::string token = getNextToken(buffer, i);
+  return std::stof(token);
+}
+
+int Parser::getNextInt(std::string &buffer, int &i)
+{
+  clearSpace(buffer, i);
+  std::string token = getNextToken(buffer, i);
+  return std::stoi(token);
 }
