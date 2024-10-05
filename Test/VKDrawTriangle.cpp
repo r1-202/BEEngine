33e38792e56c61df71684d6b5c8d384595f05dd8
r1-202 <khmkhm626@gmail.com>
2024-10-05 19:21:13 +0300

    Validation layers.

--- Test/VKDrawTriangle.cpp
+++ Test/VKDrawTriangle.cpp
@@ -19,11 +19,41 @@ const bool enable_validation_layers = false;
 const bool enable_validation_layers = true;
 #endif
 
+VkResult CreateDebugUtilsMessengerEXT(VkInstance instance,
+                                      const VkDebugUtilsMessengerCreateInfoEXT *p_create_info,
+                                      const VkAllocationCallbacks *p_allocator,
+                                      VkDebugUtilsMessengerEXT *p_debug_messenger)
+{
+  auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance,
+                                                                        "vkCreateDebugUtilsMessengerEXT");
+  if (func != nullptr)
+  {
+    return func(instance, p_create_info, p_allocator, p_debug_messenger);
+  }
+  else
+  {
+    return VK_ERROR_EXTENSION_NOT_PRESENT;
+  }
+}
+
+void DestroyDebugUtilsMessengerEXT(VkInstance instance,
+                                   VkDebugUtilsMessengerEXT debug_messenger,
+                                   const VkAllocationCallbacks *p_allocator)
+{
+  auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance,
+                                                                         "vkDestroyDebugUtilsMessengerEXT");
+  if (func != nullptr)
+  {
+    func(instance, debug_messenger, p_allocator);
+  }
+}
+
 class HelloTriangleApplication
 {
 private:
   GLFWwindow *window;
   VkInstance instance;
+  VkDebugUtilsMessengerEXT debug_messenger;
 
 public:
   void run()
@@ -46,6 +76,7 @@ private:
   void initVulkan()
   {
     createInstance();
+    setupDebugMessenger();
   }
 
   void mainLoop()
@@ -58,6 +89,11 @@ private:
 
   void cleanup()
   {
+    if (enable_validation_layers)
+    {
+      DestroyDebugUtilsMessengerEXT(instance, debug_messenger, nullptr);
+    }
+
     vkDestroyInstance(instance, nullptr);
     glfwDestroyWindow(window);
     glfwTerminate();
@@ -65,14 +101,6 @@ private:
 
   void createInstance()
   {
-    if (enable_validation_layers)
-    {
-      std::cout << "Debug mode\n";
-    }
-    else
-    {
-      std::cout << "No debug\n";
-    }
     if (enable_validation_layers && !checkValidationLayerSupport())
     {
       throw std::runtime_error("validation layers requested, but not available!");
@@ -89,25 +117,31 @@ private:
     VkInstanceCreateInfo create_info{};
     create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
     create_info.pApplicationInfo = &app_info;
-    unsigned int glfw_extension_count = 0;
-    const char **glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extension_count);
 
-    std::vector<const char *> required_extensions;
-
-    for (unsigned int i = 0; i < glfw_extension_count; ++i)
-    {
-      required_extensions.emplace_back(glfw_extensions[i]);
-    }
+    auto extensions = getRequiredExtensions();
 
 #ifdef __APPLE__
-    required_extensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
-
     create_info.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
-
 #endif
 
-    create_info.enabledExtensionCount = (unsigned int)required_extensions.size();
-    create_info.ppEnabledExtensionNames = required_extensions.data();
+    create_info.enabledExtensionCount = (unsigned int)extensions.size();
+    create_info.ppEnabledExtensionNames = extensions.data();
+
+    VkDebugUtilsMessengerCreateInfoEXT debug_create_info{};
+
+    if (enable_validation_layers)
+    {
+      create_info.enabledLayerCount = (unsigned int)validation_layers.size();
+      create_info.ppEnabledLayerNames = validation_layers.data();
+
+      populateDebugMessengerCreateInfo(debug_create_info);
+      create_info.pNext = (VkDebugUtilsMessengerCreateInfoEXT *)&debug_create_info;
+    }
+    else
+    {
+      create_info.enabledLayerCount = 0;
+      create_info.pNext = nullptr;
+    }
 
     if (vkCreateInstance(&create_info, nullptr, &instance) != VK_SUCCESS)
     {
@@ -138,6 +172,59 @@ private:
     }
 
     return true;
+  }
+
+  std::vector<char const *> getRequiredExtensions()
+  {
+    unsigned int glfw_extension_count = 0;
+    char const **glfw_extensions;
+    glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extension_count);
+    std::vector<char const *> extensions(glfw_extensions, glfw_extensions + glfw_extension_count);
+    if (enable_validation_layers)
+    {
+      extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+    }
+
+#ifdef __APPLE__
+    extensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+#endif
+
+    return extensions;
+  }
+
+  static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+      VkDebugUtilsMessageSeverityFlagBitsEXT message_severity,
+      VkDebugUtilsMessageTypeFlagsEXT message_type,
+      const VkDebugUtilsMessengerCallbackDataEXT *p_callback_data,
+      void *p_user_data)
+  {
+    std::cerr << "validation layer: " << p_callback_data->pMessage << std::endl;
+    return VK_FALSE;
+  }
+
+  void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT &create_info)
+  {
+    create_info = {};
+    create_info.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+    create_info.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
+                                  VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+                                  VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+    create_info.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+                              VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+                              VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+    create_info.pfnUserCallback = debugCallback;
+  }
+
+  void setupDebugMessenger()
+  {
+    if (!enable_validation_layers)
+      return;
+    VkDebugUtilsMessengerCreateInfoEXT create_info{};
+    populateDebugMessengerCreateInfo(create_info);
+    if (CreateDebugUtilsMessengerEXT(instance, &create_info, nullptr, &debug_messenger) != VK_SUCCESS)
+    {
+      throw std::runtime_error("failed to set up debug messenger!");
+    }
   }
 };
 
